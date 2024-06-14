6ec206147e7ea488b46e80fca49352bcaac772e7
wilyc <wilyc@LENOVO-LOQ>
2024-06-14 14:16:23 -0500

    Agregar archivos de proyecto.

---
+++ Receptor Azure/AlarmEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receptor_Azure
+{
+    public class AlarmEvent
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+}
---
+++ Receptor Azure/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receptor_Azure
+{
+    public class Ticket
+    {
+        public string TicketId { get; set; }
+        public string AlarmEventId { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
